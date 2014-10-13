7db9e45a6a4c8d4d9be817b68cc78459ad26c0b5
vakselrod <vakselrod@ws084.CINIMEX>
2014-10-13 18:33:04 +0400

    New method getResponse.

--- src/testPackage/Main.java
+++ src/testPackage/Main.java
@@ -9,12 +9,16 @@ public class Main {
         mqc.clearQueue("MQSTUB.OUT"); 
         
         String requestXMLString = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><TestingMQStubInput xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"InputFormat.xsd\">" +
- 		"<ID>This is my ID</ID> <DateTime>2014-09-17T16:16:47Z</DateTime>  <Description>Some description</Description>  <MQStubVersion>333</MQStubVersion>" + 
+ 		"<ID>This is my ID</ID> <DateTime>2014-09-17T16:16:47Z</DateTime>  <Description>Some description from NetBeans</Description>  <MQStubVersion>333</MQStubVersion>" + 
  		"<Converting>  <FormatToConvert>TestingMQStubOutput</FormatToConvert>  <IsNeededToConvert>true</IsNeededToConvert> </Converting>" +
  		"<Data>  <ElemString>Element from String</ElemString>  <ElemInt>888</ElemInt>  <ElemDateTime>2022-12-17T09:30:47Z</ElemDateTime>" +
  		"<ElemBoolean>false</ElemBoolean> </Data></TestingMQStubInput>";
         
-        XMLMessage xmlMessage = new XMLMessage(requestXMLString);
-        mqc.sendMessageSimple("MQSTUB.OUT", "MQSTUB.OUT", xmlMessage);
+        XMLMessage requestXmlMessage = new XMLMessage(requestXMLString);
+        mqc.sendMessageSimple("MQSTUB.OUT", "MQSTUB.OUT", requestXmlMessage);
+        
+        XMLMessage responseXmlMessage = mqc.getResponse("MQSTUB.IN", "MQSTUB.OUT", requestXmlMessage);
+        
+        System.out.println(responseXmlMessage.toString());
     }
 }
